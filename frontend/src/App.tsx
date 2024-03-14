009239d152b6aa65835b70f50dda69605ae4f067
hugocornellier <hugo.cornellier@gmail.com>
2024-03-14 11:29:59 -0300

    Homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,23 @@ import HomePage from "./component/HomePage";
 
 function App() {
   return (
-    <div className="bg-black min-h-screen min-w-screen">
-      <HomePage />
+    <div className="flex flex-row h-full w-full text-white">
+      <div className="pl-10 pt-5 h-screen" style={{minWidth: '300px', background: '#242222'}}>
+        <div>
+          Home
+        </div>
+        <div className="mt-10">
+          <b>
+            Games
+          </b>
+        </div>
+        <div className="mt-5">
+          Mario Kart 8
+        </div>
+      </div>
+      <div className="pl-10 pt-5 w-full">
+        Welcome to MK Records!
+      </div>
     </div>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,15 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { UserProvider } from "./context/UserProvider";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <App />
   </React.StrictMode>
 );
