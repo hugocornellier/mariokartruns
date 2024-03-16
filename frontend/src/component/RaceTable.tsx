d8efc1195e2b2edd772ae1b9b794fe47ecc69306
hugocornellier <hugo.cornellier@gmail.com>
2024-03-16 12:22:29 -0300

    New table (desktop & mobile styles)

--- frontend/src/component/RaceTable.tsx
+++ frontend/src/component/RaceTable.tsx
@@ -1,9 +1,8 @@
 import { Socket } from "socket.io-client";
 import { SocketHelper } from "../context/SocketHelper";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 
-export default function RaceTable(props:any) {
+export default function RaceTable(props: any) {
     const [raceData, setRaceData] = useState<any[]>();
 
     // Socket.io setup & calls
@@ -23,67 +22,33 @@ export default function RaceTable(props:any) {
     }, [socket]);
 
     return (
-        <div>
-            <div className="container">
-                <table className="responsive-table">
-                    <thead className="responsive-table__head">
-                    <tr className="responsive-table__row">
-                        <th className="responsive-table__head__title responsive-table__head__title--types">
-                            Time
-                        </th>
-                        <th className="responsive-table__head__title responsive-table__head__title--status">
-                            Player
-                        </th>
-                        <th className="responsive-table__head__title responsive-table__head__title--name">
-                            Date
-                        </th>
-                        <th className="responsive-table__head__title responsive-table__head__title--update">
-                            Length
-                        </th>
-                        <th className="responsive-table__head__title responsive-table__head__title--country">
-                            Country
-                        </th>
-                    </tr>
-                    </thead>
-
-                    <tbody className="responsive-table__body">
-                    {!raceData ? (
-                        <div>Loading...</div>
-                    ) : (
-                        <div>
-                            {raceData.map((record, i) => (
-                                <tr className="responsive-table__row">
-                                    <td className="responsive-table__body__text responsive-table__body__text--name">
-                                        {record.video_url != 0 && record.video_url != "0" ?
-                                            <Link target="_blank" to={record.video_url}>
-                                                { record.time }
-                                            </Link> :
-                                            <>
-                                                { record.time }
-                                            </>
-                                        }
-                                    </td>
-                                    <td className="responsive-table__body__text responsive-table__body__text--status">
-                                        <Link to={ "/mk8/player/" + record.player }>
-                                            { record.player }
-                                        </Link>
-                                    </td>
-                                    <td className="responsive-table__body__text responsive-table__body__text--types">
-                                        { record.date }
-                                    </td>
-                                    <td className="responsive-table__body__text responsive-table__body__text--update">
-                                        { record.days }
-                                    </td>
-                                    <td className="responsive-table__body__text responsive-table__body__text--country">
-                                        { record.nation }
-                                    </td>
-                                </tr>
-                            ))}
-                        </div>
-                    )}
-                    </tbody>
-                </table>
-            </div>
+        <div className="mkr-table-wrapper">
+            <table className="table">
+                <thead>
+                <th>Time</th>
+                <th>Player</th>
+                <th>Date</th>
+                <th>Country</th>
+                <th>Length</th>
+                </thead>
+                <tbody>
+                {!raceData ? (
+                    <div>Loading...</div>
+                ) : (
+                    <>
+                        {raceData.map((record, i) => (
+                            <tr>
+                                <td data-label="Time">{record.time}</td>
+                                <td data-label="Player">{record.player}</td>
+                                <td data-label="Date">{record.date}</td>
+                                <td data-label="Country">{record.nation}</td>
+                                <td data-label="Length">{record.days}</td>
+                            </tr>
+                        ))}
+                    </>
+                )}
+                </tbody>
+            </table>
         </div>
     );
 }
