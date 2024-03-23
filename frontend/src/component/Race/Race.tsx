476d09241d3ba5aa76c6619466fbe153d5e48f80
hugocornellier <hugo.cornellier@gmail.com>
2024-03-23 14:57:36 -0300

    More minor style fixes.

--- frontend/src/component/Race/Race.tsx
+++ frontend/src/component/Race/Race.tsx
@@ -13,7 +13,7 @@ export default (props: RaceProps): JSX.Element => {
     const raceName: string = Util.getRaceName();
 
     return (
-        <div className="h-full w-full text-black">
+        <div className={"h-full w-full text-black " + (!Util.pathIs('/') ? 'p-7' : '')}>
             {props.game === "mk8dx" && <Tabs cc={props.cc} />}
             {props.cc !== "all" && <RaceTitle game={props.game} raceName={raceName} />}
             <RaceTable cc={props.cc} game={props.game} raceName={raceName} />
