49c35b20435d923df34ba20a6fb8b0acde6190c0
hugocornellier <hugo.cornellier@gmail.com>
2024-04-02 11:59:22 -0300

    Typescript server testing...

--- backend/server.ts
+++ backend/server.ts
@@ -1,39 +1,18 @@
 import express = require("express");
-import { createServer } from "http";
-import { Server, Socket } from "socket.io";
+const db = require("./db/db");
+const scraper = require("./scrape/scrape_tools");
 const app = express();
-const db = require("./db/db")
-const scraper = require("./scrape/scrape_tools")
-const httpServer = createServer();
-const io = new Server(httpServer);
 
-app.use(express.static("frontend/build"));
-app.get('/', (req: express.Request, res: express.Response) => {
-    res.send("Hi");
-});
-
-io.on("connection", (socket: Socket) => {
-    console.log("Socket.io connection made successfully.");
-    socket.on("get_race_data", async (race, game, cc) => {
-        io.emit("get_race_data_ret", await db.getAllEntriesByRace(race, game, cc));
-    })
-    socket.on("get_player_data", async (player, game) => {
-        io.emit("get_player_data_ret", await db.getAllEntriesByPlayer(decodeURI(player), game), await db.getRecords(game));
-    })
-    socket.on("get_records", async (table, cc) => {
-        io.emit("get_records_ret", await db.getRecords(table, cc));
-    })
-    socket.on("get_latest_records", async (table, cc) => {
-        io.emit("get_latest_records_ret", await db.getLatestRecords());
-    })
+app.get('*', (req: express.Request, res: express.Response) => {
+    res.send('Hello, this is a basic Express server written in TypeScript!');
 });
 
 let home_path = app.settings['views'].substring(0, 5)
 const port = home_path === "/User" || home_path === "C:\\Us"
     ? 4000
     : 5000
-httpServer.listen(port, async () => {
-    console.log(`Server running => port ${port}! `)
+app.listen(port, async () => {
+    console.log(`Server running!`)
     // await scraper.scrapeAllRacesByGame(
     //     'mk8dx',
     //     false,
