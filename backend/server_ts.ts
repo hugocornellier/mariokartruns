d3258c7b49de26b5283c2e1eae29d0512b5bf227
hugocornellier <hugo.cornellier@gmail.com>
2024-04-03 14:35:45 -0300

    Separating socketHandler into it's own module.

--- backend/server_ts.ts
+++ backend/server_ts.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
 import { Server, Socket } from 'socket.io';
 import { createServer } from 'http';
-import db from './db/db_ts';
 import serverUtil from './helpers/serverUtil';
 import socketHandler from './helpers/socketHandler';
 import * as path from "path";
@@ -23,26 +22,18 @@ io.on('connection', (socket: Socket) => {
     console.log("Socket.io connection made successfully.");
 
     socket.on("get_race_data", async (race: string, game: string, cc: string) => {
-        io.emit("get_race_data_ret", await db.getAllEntriesByRace(race, game, cc));
         await socketHandler.handleGetRaceData(socket, race, game, cc);
     });
 
     socket.on("get_player_data", async (player: string, game: string) => {
-        const [playerData, records] = await Promise.all([
-            db.getAllEntriesByPlayer(decodeURI(player), game),
-            db.getRecords(game, 'all')
-        ]);
-        io.emit("get_player_data_ret", playerData, records);
         await socketHandler.handleGetPlayerData(socket, player, game);
     });
 
     socket.on("get_records", async (table: string, cc: string) => {
-        io.emit("get_records_ret", await db.getRecords(table, cc));
         await socketHandler.handleGetRecords(socket, table, cc);
     });
 
     socket.on("get_latest_records", async () => {
-        io.emit("get_latest_records_ret", await db.getLatestRecords());
         await socketHandler.handleGetLatestRecords(socket);
     });
 
@@ -55,6 +46,6 @@ const homePath: string = app.settings['views'].substring(0, 5);
 const port: number = homePath === "/User" || homePath === "C:\\Us" ? 4000 : 5000;
 
 server.listen(port, async () => {
-    console.log(`Server is running on port ${port}! [On a TS Express server :)]`);
+    console.log(`Server is running on port ${port}! [On a TS Express server :D]`);
     serverUtil.test();
 });
