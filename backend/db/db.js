143aee115b1bc2734c2bd43201f8d9a6dfe970dd
hugocornellier <hugo.cornellier@gmail.com>
2024-04-03 12:44:31 -0300

    SocketHandler module.

---
+++ backend/db/db.js
@@ -0,0 +1,289 @@
+const db_conn = require("./db_conn")
+
+module.exports = {
+
+    createTable: function createTable(tableName) {
+        db_conn.exec(`
+            CREATE TABLE ${tableName} (
+                date VARCHAR(50) NOT NULL,
+                player VARCHAR(50) NOT NULL,
+                days VARCHAR(50) NOT NULL,
+                lap1 VARCHAR(50) NOT NULL,
+                lap2 VARCHAR(50) NOT NULL,
+                lap3 VARCHAR(50) NOT NULL,
+                lap4 VARCHAR(50),
+                lap5 VARCHAR(50),
+                lap6 VARCHAR(50),
+                lap7 VARCHAR(50),
+                coins VARCHAR(50) NOT NULL,
+                shrooms VARCHAR(50) NOT NULL,
+                character VARCHAR(250) NOT NULL,
+                kart VARCHAR(250) NOT NULL,
+                tires VARCHAR(250) NOT NULL,
+                glider VARCHAR(250) NOT NULL,
+                time VARCHAR(250) NOT NULL,
+                video_url VARCHAR(250) NOT NULL,
+                controller VARCHAR(250) NOT NULL,
+                nation VARCHAR(250) NOT NULL,
+                race VARCHAR(250) NOT NULL, 
+                race_id INTEGER NOT NULL,
+                cup VARCHAR(50),
+                cc VARCHAR(50)
+            )
+        `)
+    },
+
+    // ["Game", "Track", "Record", "Player", "Date"]
+    getLatestRecords: async function getLatestRecords(game) {
+        const tables = ['mk7', 'mk8', 'mk8dx'];
+        const sqlQuery = `
+            SELECT * FROM (
+                ${tables.map(table => `
+                    SELECT '${table}' AS table_name, race, time, player, cc, date FROM ${table}
+                `).join(' UNION ALL ')}
+            ) AS combined_data
+            ORDER BY date DESC, time
+            LIMIT 50
+        `;
+
+        return new Promise((resolve, reject) => {
+            db_conn.all(sqlQuery, [], (err, rows) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+    },
+
+    // ["Game", "Track", "Record", "Player", "Date"]
+    getColeRecords: async function getColeRecords() {
+        const tables = ['mk7', 'mk8', 'mk8dx'];
+        const sqlQuery = `
+            SELECT * FROM (
+                ${tables.map(table => `
+                    SELECT * FROM ${table} WHERE player='Cole'
+                `).join(' UNION ALL ')}
+            ) AS combined_data
+            ORDER BY date DESC, time
+            LIMIT 50
+        `;
+
+        return new Promise((resolve, reject) => {
+            db_conn.all(sqlQuery, [], (err, rows) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+    },
+
+    deleteTable: async function deleteTable(table) {
+        if (await this.checkIfTableExists(table)) {
+            db_conn.exec(`
+                DROP TABLE ${table}
+            `)
+        }
+    },
+
+    insertEntry: async function insertEntry(row, table) {
+        return new Promise(async (resolve, reject) => {
+            console.log("Attempting to insert entry.")
+            const existingEntry = await this.getEntry(row, table);
+            if (existingEntry.length === 0) {
+                const columns = ['date', 'player', 'days', 'lap1', 'lap2', 'lap3', 'coins', 'shrooms', 'character',
+                    'kart', 'tires', 'glider', 'time', 'video_url', 'controller', 'nation', 'race', 'race_id', 'cc', 'cup'];
+                const babyParkColumns = (row.race !== "GCN Baby Park") ? ['lap4', 'lap5', 'lap6', 'lap7'] : [];
+                const allColumns = [...columns, ...babyParkColumns];
+                const insertQuery = `
+                    INSERT INTO ${table} (${allColumns.join(', ')}) 
+                    VALUES (${Array(allColumns.length).fill('?').join(', ')})
+                `;
+                const insertValues = allColumns.map(col => row[col]);
+                db_conn.run(insertQuery, insertValues, (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        console.log(`Inserted a row.`);
+                        resolve();
+                    }
+                });
+            } else {
+                console.log("Row already exists.");
+                reject("Row already exists.");
+            }
+        })
+    },
+
+    getEntry: async function getEntry(row, table) {
+        return new Promise((resolve, reject) => {
+            db_conn.all(
+                `SELECT * 
+                    FROM ${table} 
+                    WHERE date = ? 
+                    AND player = ? 
+                    AND lap1 = ? 
+                    AND lap2 = ? 
+                    AND lap3 = ? 
+                    AND coins = ? 
+                    AND shrooms = ? 
+                    AND character = ? 
+                    AND kart = ? 
+                    AND tires = ? 
+                    AND glider = ? 
+                    AND time = ? 
+                    AND video_url = ? 
+                    AND controller = ? 
+                    AND nation = ? 
+                    AND race = ? 
+                    AND cc = ?`,
+                [row['date'], row['player'], row['lap1'], row['lap2'], row['lap3'], row['coins'],
+                    row['shrooms'], row['character'], row['kart'], row['tires'], row['glider'], row['time'],
+                    row['video_url'], row['controller'], row['nation'], row['race'], row['cc']],
+                (err, rows) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(rows);
+                }
+            )
+        })
+    },
+
+    checkIfTableExists: async function checkIfTableExists(tableName) {
+        return new Promise((resolve, reject) => {
+            db_conn.all(
+                `SELECT name FROM sqlite_master WHERE type='table' AND name='${tableName}';`,
+                (err, rows) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve((rows.length > 0));
+                }
+            )
+        })
+    },
+
+    getRaceIdByRaceName: async function getRaceIdByRaceName(game, raceName) {
+        return new Promise((resolve, reject) => {
+            db_conn.all(
+                `SELECT * 
+                FROM ${game} 
+                WHERE race = ? 
+                ORDER BY date ASC 
+                LIMIT 1`,
+                [raceName],
+                (err, rows) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    if (rows[0] && rows[0].race_id) {
+                        resolve(rows[0].race_id);
+                    } else {
+                        reject("An error happened fetching raceID.")
+                    }
+                }
+            )
+        })
+    },
+
+    getAllEntriesByGame: async function getAllEntriesByGame(game) {
+        return new Promise((resolve, reject) => {
+            db_conn.all(
+                `SELECT * 
+                FROM ${game} 
+                ORDER BY date ASC`,
+                [],
+                (err, rows) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(rows);
+                }
+            )
+        })
+    },
+
+    getAllEntriesByRace: async function getAllEntriesByRace(race, table, cc) {
+        return new Promise((resolve, reject) => {
+            db_conn.all(
+                `SELECT * 
+                FROM ${table} 
+                WHERE race = ?
+                AND cc = ? 
+                ORDER BY time ASC`,
+                [race, cc],
+                (err, rows) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(rows);
+                }
+            )
+        })
+    },
+
+    deleteAllByRaceId: async function deleteAllByRaceId(race_id, table) {
+        return new Promise((resolve, reject) => {
+            db_conn.exec(
+                `DELETE 
+                FROM ${table} 
+                WHERE race_id = '${race_id}'`,
+                (err, rows) => {
+                    if (err) {
+                        console.log("Error: " + err)
+                        reject(err);
+                    }
+                    resolve(rows);
+                }
+            )
+        })
+    },
+
+    getAllEntriesByPlayer: async function getAllEntriesByPlayer(player, table) {
+        return new Promise((resolve, reject) => {
+            db_conn.all(
+                `SELECT * 
+                FROM ${table} 
+                WHERE player = ?
+                ORDER BY date DESC, time ASC`,
+                [player],
+                (err, rows) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(rows);
+                }
+            )
+        })
+    },
+
+    getRecords: async function getRecords(table, cc) {
+        return new Promise((resolve, reject) => {
+            db_conn.all(
+                `SELECT * 
+                FROM ${table} 
+                WHERE cc = '${cc}'
+                ORDER BY race_id, time`,
+                [], (err, rows) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        const tracksSeen = new Set();
+                        const individualRecords = [];
+                        for (const record of rows) {
+                            if (!tracksSeen.has(record.race)) {
+                                tracksSeen.add(record.race);
+                                individualRecords.push(record);
+                            }
+                        }
+                        resolve(individualRecords)
+                    }
+                }
+            )
+        })
+    }
+}
---
+++ backend/scrape/scrape_tools.js
@@ -0,0 +1,249 @@
+const fetch = require("cross-fetch");
+const parser = require("node-html-parser");
+const db = require("../db/db");
+
+const removeYouTubeLinksFromArray = a => a.filter(i => !i.includes("youtube.com/") && !i.includes("youtu.be/"));
+
+const removeDuplicatesFromArray = arr => arr.filter((value, index) => arr.indexOf(value) === index);
+
+const isNumber = value => typeof value === 'number' && isFinite(value);
+
+async function fetchHTML(url) {
+    try {
+        const res = await fetch(url);
+        if (res.status >= 400)
+            throw new Error("Bad response from server");
+        const html = await res.text();
+        if (html)
+            return html;
+        throw new Error("Error getting HTML.");
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+const getRaceRecords = async (race_url, game, race_id) => {
+    const getCellContent = (cell_count, matchingCell, cell, rowVal) => {
+        return (cell_count === matchingCell) ? cell.textContent.trim() : rowVal;
+    };
+
+    try {
+        const html = await fetchHTML(race_url);
+        if (!html) {
+            throw new Error("Error getting HTML.");
+        }
+
+        const htmlEl = parser.parse(html);
+        const race = htmlEl.querySelectorAll('h2')[0].textContent;
+        const cup = race_url.includes("&cup=") ? race_url.split("&cup=")[1] : null;
+        const cc = race_url.includes("&m=200") ? "200cc" : "150cc";
+        const table = htmlEl.querySelectorAll('table')[2];
+        const tableRows = table.querySelectorAll('tr');
+        const characterCell = game !== 'mkwii' ? 10 : 8
+        const kartCell = game !== 'mkwii' ? 11 : 9
+        const durationCell = game !== 'mkwii' ? 4 : 11
+        const nationCell = game !== 'mkwii' ? 3 : 4
+
+        const rows = tableRows.slice(1).map(tableRow => {
+            let row = { race, race_id, cup, cc };
+            let cellCount = 0;
+            for (const cell of tableRow.querySelectorAll('td')) {
+                row['date'] = getCellContent(cellCount, 0, cell, row.date);
+                row['player'] = getCellContent(cellCount, 2, cell, row.player);
+                row['days'] = getCellContent(cellCount, durationCell, cell, row.days);
+                row['lap1'] = getCellContent(cellCount, 5, cell, row.lap1);
+                row['lap2'] = getCellContent(cellCount, 6, cell, row.lap2);
+                row['lap3'] = getCellContent(cellCount, 7, cell, row.lap3);
+                row['coins'] = (game !== 'mkwii'
+                    ? getCellContent(cellCount, 8, cell, row.coins)
+                    : '');
+                row['shrooms'] = (game !== 'mkwii'
+                    ? getCellContent(cellCount, 9, cell, row.shrooms)
+                    : '');
+                row['character'] = getCellContent(cellCount, characterCell, cell, row.character);
+                row['kart'] = getCellContent(cellCount, kartCell, cell, row.kart);
+                row['tires'] = getCellContent(cellCount, 12, cell, row.tires);
+                row['glider'] = (game !== 'mkwii'
+                    ? getCellContent(cellCount, 13, cell, row.glider)
+                    : '');
+                row['controller'] = (game === 'mkwii'
+                    ? getCellContent(cellCount, 10, cell, row.controller)
+                    : row.controller);
+
+                if (cellCount === 1) {
+                    const el = parser.parse(cell.innerHTML);
+                    const a_el = el.querySelector('a');
+                    const img_el = el.querySelector('img');
+                    const rawAttrs = a_el ? a_el.rawAttrs : null;
+                    const rawAttrsImg = img_el ? img_el.rawAttrs : null;
+                    row['time'] = cell.textContent.trim();
+                    row['video_url'] = rawAttrs ? rawAttrs.split('"')[1].trim() : 0;
+                    row['controller'] = rawAttrsImg ? rawAttrsImg.split('"')[1].trim() : "default";
+                } else if (cellCount === nationCell) {
+                    const el = parser.parse(cell.innerHTML);
+                    const img_el = el.querySelector('img');
+                    const rawAttrsImg = img_el ? img_el.rawAttrs : null;
+                    row['nation'] = rawAttrsImg ? rawAttrsImg.split('"')[1].trim() : 0;
+                }
+                cellCount++;
+            }
+            const dateIsNan = isNaN(new Date(row['date']).getTime());
+            row['date'] = (!dateIsNan) ? row['date'] : '0: ' + row['date'];
+            return row;
+        });
+
+        return rows.filter(value => Object.keys(value).length !== 0);
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+module.exports = {
+    getAndInsertRecords: async function getAndInsertRecords(race_url, table, race_id) {
+        try {
+            console.log("Fetching records at URL: " + race_url);
+            if (race_id === null) {
+                let raceName = race_url.split("?track=")[1].split("+").join(' ')
+                if (raceName.endsWith('&m=200')) {
+                    raceName = raceName.slice(0, -6)
+                }
+                raceName = decodeURI(raceName.trim())
+                const raceID = await db.getRaceIdByRaceName(table, raceName)
+                if (isNumber(raceID)) {
+                    race_id = raceID;
+                    console.log(`Found race ID: ${race_id}`)
+                } else {
+                    return;
+                }
+            }
+            const rows = await getRaceRecords(race_url, table, race_id);
+            console.log("Race records received! Attempting to insert all rows.");
+            console.log(rows);
+            const tableExists = await db.checkIfTableExists(table);
+            if (!tableExists) {
+                await db.createTable(table);
+            }
+            for (const row of rows) {
+                try {
+                    await db.insertEntry(row, table);
+                } catch (e) {
+                    console.log("Error: " + e.message);
+                }
+            }
+            console.log("Done inserting...");
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    },
+
+    scrapeAllRacesByGame: async function scrapeAllRacesByGame(game, deleteTable, startingRaceID) {
+        if (deleteTable) {
+            await db.deleteTable(game)
+        }
+        let race_id = game === 'mk8dx' ? 1.01 : 1
+        for (const url of await this.getRaceURLs(game)) {
+            if (race_id >= startingRaceID) {
+                await this.getAndInsertRecords(url, game, Math.floor(race_id))
+            }
+            race_id = race_id + (game === 'mk8dx' ? 0.5 : 1)
+        }
+    },
+
+    scrapeHomePage: async function scrapeHomePage() {
+        return new Promise(async (resolve, reject) => {
+            const html = parser.parse(await fetchHTML('https://mkwrs.com/'))
+            const wrTable = html.querySelectorAll('.wr')[0];
+            const tableRows = wrTable.querySelectorAll('tr').slice(1) // Remove header row
+            for (const tableRow of tableRows) {
+                console.log()
+                const tableDataCells = tableRow.querySelectorAll('td').slice(1) // Removes date row - we won't use it
+                let count = 1
+                let toScrape = false
+                let game = null
+                let raceURL = null
+                for (const tableDataCell of tableDataCells) {
+                    if (count === 1) {
+                        game = tableDataCell.querySelector('a')._attrs.href.slice(0, -1).toLowerCase()
+                        if (['mk7', 'mk8', 'mk8dx'].includes(game)) {
+                            toScrape = true
+                        }
+                    } else if (count === 2 && toScrape)  {
+                        raceURL = tableDataCell.querySelector('a')._attrs.href
+                        raceURL = "https://mkwrs.com/" + raceURL
+                        await this.getAndInsertRecords(
+                            raceURL,
+                            game,
+                            null
+                        )
+                    }
+                    count += 1
+                }
+            }
+        })
+    },
+
+    getMKWiiUrls: async function getMKWiiUrls() {
+        return new Promise(async (resolve, reject) => {
+            const html = parser.parse(await fetchHTML('https://mkwrs.com/mkwii/'))
+            const wrTables = html.querySelectorAll('.wr');
+            let race_urls = []
+            for (const wrTable of wrTables) {
+                const tableRows = wrTable.querySelectorAll('tr').slice(1) // Remove header row
+                for (const tableRow of tableRows) {
+                    const tableDataCells = tableRow.querySelectorAll('td')
+                    const linkCell = tableDataCells[0]
+                    if (linkCell) {
+                        link = linkCell.querySelector('a');
+                        if (link && '_rawAttrs' in link && 'href' in link._rawAttrs) {
+                            let href = "https://mkwrs.com/mkwii/" + link._rawAttrs.href
+                            race_urls.push(href)
+                        }
+                    }
+                }
+            }
+            resolve(race_urls)
+        })
+    },
+
+    getRaceURLs: async function getRaceURLs(game) {
+        return new Promise(async (resolve, reject) => {
+            if (game === "mkwii") {
+                console.log()
+                resolve(await this.getMKWiiUrls())
+            }
+            race_urls = []
+            base_url = `https://mkwrs.com/${game}/`
+            let cup = null
+            const html = await fetchHTML(base_url)
+            if (!html)
+                reject("Error getting HTML.")
+            const tables = parser.parse(html).querySelectorAll('table')
+            const table_els = [tables[0], tables[1]]
+            for (var table of table_els) {
+                let tr_count = 0
+                for (var tr of table.querySelectorAll('tr')) {
+                    const track_cell = tr.querySelectorAll('td')[0]
+                    if (track_cell) {
+                        a_el = track_cell.querySelector('center')
+                        if (a_el) {
+                            let cup_data= a_el.innerHTML.split('src="cups/')[1].trim()
+                            cup_data = cup_data.split(".webp")[0].trim()
+                            const cup_first_letter_capitalized = cup_data.charAt(0).toUpperCase() + cup_data.slice(1)
+                            cup = cup_first_letter_capitalized + ' '
+                            console.log(cup)
+                        }
+                        a_el = track_cell.querySelector('a')
+                        if (a_el) {
+                            race_urls.push(base_url + a_el.rawAttrs.split('"')[1].trim() + "&cup=" + cup)
+                        }
+                    }
+                }
+            }
+            if (!Array.isArray(race_urls)) {
+                reject("Not array")
+            }
+            resolve(removeYouTubeLinksFromArray(removeDuplicatesFromArray(race_urls)))
+        })
+    }
+}
