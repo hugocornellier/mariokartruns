0115f1f86e0b68341b5b1071f9c5e6e88247f578
hugocornellier <hugo.cornellier@gmail.com>
2024-04-03 12:03:52 -0300

    Still trying to fix the TS config :(

--- backend/server_ts.ts
+++ backend/server_ts.ts
@@ -1,43 +1,40 @@
-import { Request, Response } from 'express';
+import express = require('express');
 import { Server, Socket } from 'socket.io';
-import { createServer } from 'http';
-import db from './db/db_ts';
+import * as http from 'http';
+import db = require('./db/db');
 
-const app = require('express')();
-const server = createServer(app);
-const io: Server = new Server(server);
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.use((_req: Request, res: Response) => {
-    res.sendFile("index.html", { root: "../frontend/build" });
+app.use((req, res) => {
+    res.sendFile("index.html", {root: "../frontend/build"});
 });
 
 io.on('connection', (socket: Socket) => {
     console.log("Socket.io connection made successfully.");
-    socket.on("get_race_data", async (race: string, game: string, cc: string) => {
+    socket.on("get_race_data", async (race, game, cc) => {
         io.emit("get_race_data_ret", await db.getAllEntriesByRace(race, game, cc));
-    });
-    socket.on("get_player_data", async (player: string, game: string) => {
-        const [playerData, records] = await Promise.all([
-            db.getAllEntriesByPlayer(decodeURI(player), game),
-            db.getRecords(game, 'all')
-        ]);
-        io.emit("get_player_data_ret", playerData, records);
-    });
-    socket.on("get_records", async (table: string, cc: string) => {
+    })
+    socket.on("get_player_data", async (player, game) => {
+        io.emit("get_player_data_ret", await db.getAllEntriesByPlayer(decodeURI(player), game), await db.getRecords(game));
+    })
+    socket.on("get_records", async (table, cc) => {
         io.emit("get_records_ret", await db.getRecords(table, cc));
-    });
-    socket.on("get_latest_records", async () => {
+    })
+    socket.on("get_latest_records", async (table, cc) => {
         io.emit("get_latest_records_ret", await db.getLatestRecords());
-    });
+    })
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
 });
 
-const homePath: string = app.settings['views'].substring(0, 5);
-const port: number = homePath === "/User" || homePath === "C:\\Us" ? 4000 : 5000;
+let home_path = app.settings['views'].substring(0, 5)
+const port: number = home_path === "/User" || home_path === "C:\\Us"
+    ? 4000
+    : 5000
 
-server.listen(port, async () => {
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-    console.log(await db.getLatestRecords())
 });
