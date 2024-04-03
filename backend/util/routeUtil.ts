a0feaa3a7f4d80621e4c61fa2118f3d5209f3332
hugocornellier <hugo.cornellier@gmail.com>
2024-04-03 16:30:05 -0300

    Path fix.

--- backend/util/routeUtil.ts
+++ backend/util/routeUtil.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import * as path from "path";
 
-const buildPath: string = path.join(__dirname, "../frontend/build");
+const buildPath: string = path.join(__dirname, "../../frontend/build");
 console.log(buildPath);
 
 export function handleGetRequest(_req: Request, res: Response) {
