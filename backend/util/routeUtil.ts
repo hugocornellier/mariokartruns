c57c2cd85b41b800f83909b9a2a56e3e19ace963
hugocornellier <hugo.cornellier@gmail.com>
2024-04-03 16:29:28 -0300

    Further obfuscation of server.ts.

--- backend/util/routeUtil.ts
+++ backend/util/routeUtil.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import * as path from "path";
 
 const buildPath: string = path.join(__dirname, "../frontend/build");
+console.log(buildPath);
 
 export function handleGetRequest(_req: Request, res: Response) {
     res.sendFile("index.html", {
